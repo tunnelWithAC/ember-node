a8258b8b29387d61a135815996608aa8eb5e884f
conall.daly@insight-centre.org <conall.daly@insight-centre.org>
2017-08-31 16:43:25 +0100

    More login/session service

--- mongo-sample/app/controllers/login.js
+++ mongo-sample/app/controllers/login.js
@@ -3,7 +3,19 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
 
+  loginName: null,
+  password: null,
   actions: {
+    authenticate(){
+      this.get('session').authenticate(
+        this.get('loginName'),
+        this.get('password')).then( ()=> {
+          alert('Logged in');
+          //this.transitionTo('post');
+        }, (err) => {
+          alert("Error: " + err.responseText);
+        });
+    },
     login() {
       let { identification, password } = this.getProperties('email', 'password');
       this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
