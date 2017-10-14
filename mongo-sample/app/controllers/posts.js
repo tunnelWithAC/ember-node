a5d6ae287283ce679d74a95afb1c9761d0b1b8fd
Conall <c.daly19@nuigalway.ie>
2017-10-14 13:30:59 +0100

    updates

--- mongo-sample/app/controllers/posts.js
+++ mongo-sample/app/controllers/posts.js
@@ -1,5 +1,6 @@
 //cont { store } = this
 import Ember from 'ember';
+import EmberObject, { computed } from '@ember/object'
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
@@ -8,10 +9,42 @@ export default Ember.Controller.extend({
   disableUp: false,
   disableDown: false,
 
+  todos: null,
+
+  init() {
+    this.set('todos', [
+      EmberObject.create({ isDone: true }),
+      EmberObject.create({ isDone: false }),
+      EmberObject.create({ isDone: true }),
+    ]);
+  },
+
+  selectedTodo: null,
+  indexOfSelectedTodo: computed('selectedTodo', 'todos.[]', function() {
+    console.log(this.get('selectedTodo'));
+    return this.get('todos').indexOf(this.get('selectedTodo'));
+  }),
 
   postvotes: Ember.observer('uservotes', 'posts', function() {
+    console.log("Posts: ", this.get('posts'));
+    let posts = this.get('posts');
+    let uservotes = this.get('uservotes');
+    var uv = Ember.A();
+    if(uservotes){
+      uservotes.forEach(function(uservote){
+        console.log(uservote.id);
+        uv.pushObject(uservote.id);
+      });
+    }
+    console.log(uv);
+    return "test";
 
   }),
+
+  isTrue: function(param) {
+    console.log(param);
+    return false;
+  }.property('uservotes'),
 
   actions: {
     saveComment(post, comment, index){
@@ -20,76 +53,76 @@ export default Ember.Controller.extend({
       Ember.Logger.log(posts.objectAt(index));
       //this.set(posts.objectAt(index).comment, "");
       /*return $.ajax({
-        method: 'POST',
-        url: 'http://localhost:8081/api/comments',
-        data: {
-          vote: vote,
-          user: this.get('session').userID,
-          post: post.id
-        }
-      }).then((resp) => {
-        this.set('disableUp', resp.data.status);
-        if(resp.data.status){
-          post.incrementProperty('votes');
-        }
-      });*/
-    },
-    upvote(post, vote){
-      return $.ajax({
-        //host: 'http://localhost:8081',
-        method: 'POST',
-        url: 'http://localhost:8081/api/upvote',
-        data: {
-          vote: vote,
-          user: this.get('session').userID,
-          post: post.id
-        }
-      }).then((resp) => {
-        this.set('disableUp', resp.data.status);
-        if(resp.data.status){
-          post.incrementProperty('votes');
-        }
-      });
-    },
-
-    downvote(post, vote){
-     $.ajax({
-        //host: 'http://localhost:8081',
-        method: 'POST',
-        url: 'http://localhost:8081/api/vote',
-        data: {
-          user: this.get('session').userID,
-          post: post.id
-        }
-      }).then((resp) => {
-        this.set('disableDown', resp.data.status);
-        if(resp.data.status){
-          post.decrementProperty('votes');
-        }
-      });
-    },
-    addPost() {
-      /*
-      get index of each hashtag
-      check if each hashtag is followed by a letter/number
-      // https://stackoverflow.com/questions/18042133/check-if-input-is-number-or-letter-javascript
-      get each word that is hashtagged
-      surround each word with link
-      */
-      var post = this.store.createRecord('post', {
-        content: this.get('post'),
-        //image: file,
-        author: '4 Sept',
-        votes: 0
-      });
-      post.save();
-    },
-    addComment(){
-      // var user = this.get.('session').currentUser();
-      var comment = this.store.createRecord('comment', {
-        comment: 'test'
-      });
-      comment.save();
+      method: 'POST',
+      url: 'http://localhost:8081/api/comments',
+      data: {
+      vote: vote,
+      user: this.get('session').userID,
+      post: post.id
     }
-  }
+  }).then((resp) => {
+  this.set('disableUp', resp.data.status);
+  if(resp.data.status){
+  post.incrementProperty('votes');
+}
+});*/
+},
+upvote(post, vote){
+  return $.ajax({
+    //host: 'http://localhost:8081',
+    method: 'POST',
+    url: 'http://localhost:8081/api/upvote',
+    data: {
+      vote: vote,
+      user: this.get('session').userID,
+      post: post.id
+    }
+  }).then((resp) => {
+    this.set('disableUp', resp.data.status);
+    if(resp.data.status){
+      post.incrementProperty('votes');
+    }
+  });
+},
+
+downvote(post, vote){
+  $.ajax({
+    //host: 'http://localhost:8081',
+    method: 'POST',
+    url: 'http://localhost:8081/api/vote',
+    data: {
+      user: this.get('session').userID,
+      post: post.id
+    }
+  }).then((resp) => {
+    this.set('disableDown', resp.data.status);
+    if(resp.data.status){
+      post.decrementProperty('votes');
+    }
+  });
+},
+addPost() {
+  /*
+  get index of each hashtag
+  check if each hashtag is followed by a letter/number
+  // https://stackoverflow.com/questions/18042133/check-if-input-is-number-or-letter-javascript
+  get each word that is hashtagged
+  surround each word with link
+  */
+  var post = this.store.createRecord('post', {
+    content: this.get('post'),
+    //image: file,
+    author: '4 Sept',
+    votes: 0
+  });
+  post.save();
+},
+addComment(){
+  // var user = this.get.('session').currentUser();
+  var comment = this.store.createRecord('comment', {
+    comment: 'test'
+  });
+  comment.save();
+}
+}
 });
--- mongo-sample/app/routes/posts.js
+++ mongo-sample/app/routes/posts.js
@@ -2,61 +2,86 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
-/*
-Snippets:
+  /*
+  Snippets:
 
-includes(){
+  includes(){
   return array.mapBy('content').includes(param)
 }
 
 */
 
 model() {
-	//console.log(this.store.findAll('post'));
-    return Ember.RSVP.hash({
+  //console.log(this.store.findAll('post'));
+  return Ember.RSVP.hash({
 
-      posts: this.store.findAll('post'),
-      //return uservotes for only currentUser
-      uservotes: this.store.findAll('uservote')
-    });
-  },
+    posts: this.store.findAll('post'),
+    //return uservotes for only currentUser
+    uservotes: this.store.findAll('uservote')
+  });
+},
 
-  setupController(controller, model) {
-    this._super(...arguments);
-    Ember.set(controller, 'posts', model.posts);
-    Ember.set(controller, 'uservotes', model.uservotes);
+setupController(controller, model) {
+  this._super(...arguments);
+  Ember.set(controller, 'posts', model.posts);
+  Ember.set(controller, 'uservotes', model.uservotes);
 
-    let uservotes  = controller.get('uservotes').content ;
-    let posts = controller.get('posts').content ;
+  let uservotes  = controller.get('uservotes').content ;
+  var posts = controller.get('posts').content ;
+  uservotes.objectAt(0)._data.post = "tst";
+  //console.log("a", uservotes.objectAt(0)._data.post);
+  //uservotes.objectAt(0).push({vote: 'up'});
+  //Ember.Logger.log("posts: ",posts);
+  //Ember.Logger.log("uservotes", uservotes);
 
-    Ember.Logger.log("posts: ",posts);
-    Ember.Logger.log(uservotes);
 
+  if(uservotes && posts){
+    //Ember.Logger.log("No. uv: ", uservotes.length);
+    //Ember.Logger.log("No. posts: ", posts.length);
+    //console.log("uv", uservotes.objectAt(0)._data);
+    var firstUservote = uservotes.objectAt(0)._data.post;
+    //console.log("Finding post: ", firstUservote);
+    for(var i = 0; i < 3; i++){
+      var post_id = uservotes.objectAt(i)._data.post;
+      //console.log("Finding post: ", post_id);
+      posts.forEach(function(post){
+        //console.log(post.id);
+        if(post.id === post_id){
+          //console.log("Match for " + i + " found");
+          var vote_value = uservotes.objectAt(i)._data.value;
+          if(vote_value === 1){
+            //console.log("Upvote");
 
-    /*if(uservotes){
-      Ember.Logger.log("No. uv: ", this.get('uservotes').content.length);
-      Ember.Logger.log("No. posts: ", posts.content.length);
-      //console.log("uv", uservotes.objectAt(0).data);
-      uservotes.forEach(function(uservote, index) {
-          console.log("First post = ", posts.objectAt(i).post);
-          console.log("Looking for ", uservote.data.post);
-          for(var i=0; i<posts.content.length; i++){
-            console.log(" Current: " + posts.objectAt(i).post);
-            if(uservote.data.post === posts.objectAt(i).post){
-              console.log("match found");
-              console.log(posts.objectAt(i).post);
-              //pushObject
-            }
+            //console.log(uservotes.objectAt(i));
           }
-          /*
-          find the post associated with each vote
-            > > >push the value of the vote into the post
-          //
-          //mutableComments.pushObject(story);
-
+          else {
+            //console.log("Downvote");
+          }
+        }
       });
-    }*/
 
+    }
+
+    /*uservotes.forEach(function(uservote, index) {
+    console.log("First post = ", posts.objectAt(i).post);
+    console.log("Looking for ", uservote.data.post);
+    for(var i=0; i<posts.content.length; i++){
+    console.log(" Current: " + posts.objectAt(i).post);
+    if(uservote.data.post === posts.objectAt(i).post){
+    console.log("match found");
+    console.log(posts.objectAt(i).post);
+    //pushObject
   }
+}
+/*
+find the post associated with each vote
+> > >push the value of the vote into the post
+//
+//mutableComments.pushObject(story);
+
+});*/
+}
+
+}
 
 });
